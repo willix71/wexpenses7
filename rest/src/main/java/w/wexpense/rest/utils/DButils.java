075b45ab0d0095aa804f4158e2e92bb328153070
willix <willix71@gmail.com>
2015-12-14 21:43:27 +0100

    added a tool to fetch a dbable version

---
+++ rest/src/main/java/w/wexpense/rest/utils/DButils.java
@@ -0,0 +1,30 @@
+package w.wexpense.rest.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import w.wexpense.model.DBable;
+
+@Component
+public class DButils {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public Long getVersion(Class<? extends DBable<?>> entityClass, Object id) {
+		return getVersion(entityClass.getSimpleName(), id);
+	}
+	public Long getVersion(String entityClass, Object id) {
+		try {
+			TypedQuery<Long> query = entityManager.createQuery("SELECT version FROM " + entityClass + " WHERE id = :id", Long.class);
+			query.setParameter("id", id);		
+			return query.getSingleResult();
+		} catch(NoResultException nre) {
+			return null; // to be consistent with JpaRepoDaoService
+		}
+	}
+}
